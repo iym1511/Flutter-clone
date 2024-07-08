0ddde760a52dafa1c7afd3b7b52ac6c93e74407c
문일윤 <iym1511@naver.com>
2024-07-08 20:51:24 +0900

    FEAT : Scaffold, 가로세로 배치

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,47 @@ class MyApp extends StatelessWidget {
       // home: Text('안녕') // 글자위젯
       // home : Icon(Icons.star) // 아이콘 위젯 (플러터 홉페이지에 명령어 있음)
       // home: Image.asset('assets/img.png') // 이미지위젯 (디렉토리 명 asset으로 설정)
-      home: Center( // Container위젯 안에다 Container도 넣을 수 있다.
-        child: Container(width: 50, height: 50, color: Colors.blue),
+      // home: Center( // Container위젯 안에다 Container도 넣을 수 있다.
+      //   child: Container(width: 50, height: 50, color: Colors.blue),
+      // )
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+        ),
+        body: Container(
+          child: Container(
+            child: Text('앱임'),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // 그림자 포지션 설정
+              ),
+            ],
+          ),
+            child: BottomAppBar(
+              color: Colors.white, // BottomAppBar의 배경색을 투명으로 설정
+              child: Container(
+                height: 50, // 바닥바 높이 설정
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.call),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ],
+              ),
+            ),
+          ),
+        ),
       )
     );
   }
